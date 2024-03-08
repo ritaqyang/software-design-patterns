c13c68c2c03ddc75fa2f5cd7d9a46b7bc9c29765
ritaqyang <ritaqyang@gmail.com>
2024-03-08 17:53:34 -0500

    lab2 review fixed

--- src/ca/mcgill/cs/swdesign/iterator/streaming/GenreFilter.java
+++ src/ca/mcgill/cs/swdesign/iterator/streaming/GenreFilter.java
@@ -3,7 +3,7 @@ package ca.mcgill.cs.swdesign.iterator.streaming;
 public class GenreFilter implements Filter {
 
     public boolean filter(String searchString, Entertainment e){
-        if (e.getGenre() == searchString){
+        if (e.getGenre().toString() == searchString){
             return true;
         }
 
