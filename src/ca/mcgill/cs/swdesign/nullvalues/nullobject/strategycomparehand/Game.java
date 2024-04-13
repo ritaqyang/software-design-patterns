e48e4b62f719c9939eba8bf8333d2482be80f5ab
ritaqyang <ritaqyang@gmail.com>
2024-04-13 11:52:08 -0400

    null object solution to handle null comparator

---
+++ src/ca/mcgill/cs/swdesign/nullvalues/nullobject/strategycomparehand/Game.java
@@ -0,0 +1,45 @@
+package ca.mcgill.cs.swdesign.nullvalues.nullobject.strategycomparehand;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class Game
+{
+
+    private  List<Hand> allHands = new ArrayList<>();
+
+    public List<Hand> getAllHands() {
+        return allHands;
+    }
+
+    public void setAllHands(List<Hand> allHands) {
+        this.allHands = allHands;
+    }
+
+    public void printAllHands(){
+        for (Hand h : allHands){
+            System.out.println(h);
+        }
+    }
+    public static void main(String[] args){
+        Hand hand1 = new Hand(15, Card.createBySuitComparator());
+        Hand hand2 = new Hand(15, Card.createBySuitComparator());
+        Hand hand3 = new Hand(8);
+        Hand hand4 = new Hand(9);
+
+        Game g = new Game();
+        g.setAllHands( Arrays.asList(hand1,hand2,hand3,hand4));
+
+        System.out.println("printing all hands, first two sorted by suit, second 2 sorted by rank ");
+        g.printAllHands();
+
+
+
+
+
+    }
+
+
+
+
+}
