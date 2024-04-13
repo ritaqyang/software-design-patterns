78cf353468847206a73a6501f87174c5322db12f
ritaqyang <ritaqyang@gmail.com>
2024-04-13 14:40:17 -0400

    stack tests finished

--- src/ca/mcgill/cs/swdesign/unittesting/exercises/StackTest.java
+++ src/ca/mcgill/cs/swdesign/unittesting/exercises/StackTest.java
@@ -3,9 +3,11 @@ package ca.mcgill.cs.swdesign.unittesting.exercises;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Write unit test for methods push and pop of class Stack. Use a fixture that you
@@ -22,12 +24,25 @@ public class StackTest {
         assertEquals(5, aStack.peek());
 
     }
-
     @Test
     void testPushToNonEmptyStack(){
         aStack.push(6);
         assertEquals(5, aStack.push(5));
         assertEquals(5, aStack.peek());
+    }
+    @Test
+    void testPopFromNonEmptyStack(){
+        aStack.push(6);
+        aStack.push(5);
+        assertEquals(5, aStack.peek());
+        assertEquals(5, aStack.pop());
+        assertEquals(6, aStack.peek());
+        assertEquals(6, aStack.pop());
+    }
+
+    @Test
+    void testPopFromEmptyStack(){
+        assertThrows(EmptyStackException.class, ()->{aStack.pop(); });
     }
 
 
